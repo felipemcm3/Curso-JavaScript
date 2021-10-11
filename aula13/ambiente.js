135e7df381ba02c51e805f51f6d577716a1d75d3
Felipe <felipemcm3@gmail.com>
2021-10-11 08:49:55 -0300

    ambiente

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+/*x = 0
+do{
+    console.log(`Passo ${x}`)
+    x++
+}while(x <= 6)
+
+y = 10
+
+while(y >= 0){
+    console.log(`Passo ${y}`)
+    y--
+}
+*/
+
+for(var o = 1;o <= 6; o++ ){
+    console.log(`Passo ${o}`)
+}
