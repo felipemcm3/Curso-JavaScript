4ff2448e261be017e2bd3bda9ae0a4b426f9470f
Felipe <felipemcm3@gmail.com>
2021-10-12 09:51:21 -0300

    dados

--- aula13/dados.js
+++ aula13/dados.js
@@ -1,16 +1,33 @@
 function conta(){
-    var num = window.document.getElementById('primeiro').value
-    var ult = window.document.getElementById('ultimo').value
-    var pass = window.document.getElementById('passo').value
+    var num = window.document.getElementById('primeiro')
+    var ult = window.document.getElementById('ultimo')
+    var pass = window.document.getElementById('passo')
     var res = window.document.getElementById('resultado')
-    var outra = document.createElement('o')
-    var n = Number(num)
-    var u = Number(ult)
-    var p = Number(pass)
-
-    while(n<=u){
-        res.appendChild(outra);
-        outra.textContent = "ola mundo"
+    //Validando dados
+    if(num.value.length == 0 || ult.value.length == 0 || pass.value.length == 0 || pass.value <= 0){
+        res.innerHTML = `Impossivel contar`
+    }
+    else{
+        res.innerHTML = 'Contando:'
+        //Transformando em Numeros
+        let n = Number(num.value)
+        let u = Number(ult.value)
+        let p = Number(pass.value)
+        //Contagem crescente
+        if(n <= u){
+            while(n <= u){
+                res.innerHTML += `\u{1F449}${n} `
+                n += p
+            }
+        }//Contagem decrescente
+        else{
+            for(let c = n; c >= u; c -= p){
+                res.innerHTML += `\u{1F449}${c}`
+            }
+            
+        }
+        res.innerHTML += `\u{1F44D}`
+        
     }
     
 }
