91014fbc3aa135c5bff228cec743deb03a140bfd
Felipe <felipemcm3@gmail.com>
2021-10-14 09:10:49 -0300

    tabuada pronta

--- tabuada/script.js
+++ tabuada/script.js
@@ -1,3 +1,19 @@
-var n = window.document.getElementById('numero')
+function mostrar(){
+    var n = window.document.getElementById('numero');
+    var res = window.document.getElementById('resposta');
 
-if(n.value.length == 0)
+    if(n.value.length == 0 || n.value < 0){
+        res.innerHTML = 'Informe um número'
+    }
+    else{
+        var o = Number(n.value)
+        var c = 1
+        res.innerHTML = ``
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${o} x ${c} = ${o*c}`
+            res.appendChild(item)
+            c++
+        }
+    }
+}
