22209de0acb458abb0b365834437c3d3295c81b6
Felipe <felipemcm3@gmail.com>
2021-10-14 10:28:07 -0300

    array

---
+++ aula15/array.js
@@ -0,0 +1,14 @@
+let num = []
+
+for(var c = 0; c < 10; c++){
+    if(c % 2 ==0){
+        num[c] = c + 3
+    }else{
+        num[c] = c
+    }
+    
+}
+console.log(`${num}`)
+console.log(`Esse vetor tem ${num.length} elementos`)
+num.sort()
+console.log(`Esse vetor com elementos em ordem ${num}`) 
